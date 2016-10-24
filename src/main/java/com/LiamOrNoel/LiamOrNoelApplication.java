a1d6dacae9d26917206dc354de93f7c04aac3e6d
Christoffer Skeppstedt <chris.skeppstedt@klarna.com>
2016-10-24 21:13:21 +0200

    Bootstrap maven project

---
+++ src/main/java/com/LiamOrNoel/LiamOrNoelApplication.java
@@ -0,0 +1,12 @@
+package com.LiamOrNoel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiamOrNoelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiamOrNoelApplication.class, args);
+	}
+}
---
+++ src/test/java/com/LiamOrNoel/LiamOrNoelApplicationTests.java
@@ -0,0 +1,16 @@
+package com.LiamOrNoel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LiamOrNoelApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
